d97372b4977543b402cd49df807e8cfecc47e310
Siva Jothi <89371435+Siva901@users.noreply.github.com>
2024-03-01 10:31:56 +0530

    checking

---
+++ Mobile.java
@@ -0,0 +1,20 @@
+package week1.day3;
+
+public class Mobile {
+	
+	public void Iphone() {
+		System.out.println("Iphone XR");
+	}
+	public void Android() {
+		System.out.println("samsung");
+	}
+
+	public static void main(String[] args) {
+		//classname obj=new classname();
+		Mobile mb=new Mobile();
+		mb.Android();
+		mb.Iphone();
+
+	}
+
+}
